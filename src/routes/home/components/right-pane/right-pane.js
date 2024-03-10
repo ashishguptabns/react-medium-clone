ef5cbd19b55806a5555f82713a73acb4cd843fe5
ashishguptabns <ashishguptabns@gmail.com>
2024-03-11 03:29:25 +0530

    DSA

--- src/routes/home/components/right-pane/right-pane.js
+++ src/routes/home/components/right-pane/right-pane.js
@@ -1,3 +1,26 @@
+import { useEffect, useState } from 'react'
+import problems from '../../../../lib/dsa-problems'
+import styled from "styled-components";
+
+const Container = styled.div`
+    padding: 40px;
+    line-height: 1.3;
+    white-space: pre-wrap;
+    width: 40%;
+    @media (max-width: 700px){
+        display: none;
+    }
+`
 export const HomeRightPane = () => {
-    return <div></div>
+    const [problem, setProblem] = useState()
+
+    useEffect(() => {
+        const randomNum = Math.random() * problems.length
+        setProblem(problems[Math.floor(randomNum)])
+    }, [])
+
+    return <Container>
+        <h2>Solve this Problem</h2>
+        {problem}
+    </Container>
 }
