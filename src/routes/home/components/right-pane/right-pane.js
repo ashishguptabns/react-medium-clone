b12a436e13188b5730be1b7961bf854d7a6ddd5f
ashishguptabns <ashishguptabns@gmail.com>
2024-03-11 12:01:08 +0530

    handleRefresh

--- src/routes/home/components/right-pane/right-pane.js
+++ src/routes/home/components/right-pane/right-pane.js
@@ -2,6 +2,22 @@ import { useEffect, useState } from 'react'
 import problems from '../../../../lib/dsa-problems'
 import styled from "styled-components";
 
+export const RefreshButton = styled.div`
+    width: 70px;
+    background: green;
+    color: white;
+    font-size: 14px;
+    border-radius: 10px;
+    padding: 6px;
+    margin-left: 40px;
+    cursor: pointer;
+    text-align: center;
+    height: fit-content;
+    align-self: center;
+`
+const ProblemContainer=styled.div`
+    display: flex;
+`
 const Container = styled.div`
     padding: 40px;
     line-height: 1.3;
@@ -13,14 +29,22 @@ const Container = styled.div`
 `
 export const HomeRightPane = () => {
     const [problem, setProblem] = useState()
+    const [refresh, setRefresh] = useState()
 
     useEffect(() => {
         const randomNum = Math.random() * (problems.length - 1)
         setProblem(problems[Math.floor(randomNum)])
-    }, [])
+    }, [refresh])
 
-    return <Container>
-        <h2>Solve this Problem</h2>
+    const handleRefresh = () => {
+        setRefresh(!refresh)
+    }
+
+return <Container>
+        <ProblemContainer>
+            <h2>Solve this Problem</h2>
+            <RefreshButton onClick={handleRefresh}>Refresh</RefreshButton>
+        </ProblemContainer>
         {problem}
     </Container>
 }
