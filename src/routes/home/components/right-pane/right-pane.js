6b5fbfe52a1f74195e80e633c8917f961a39507a
ashishguptabns <ashishguptabns@gmail.com>
2024-03-15 18:36:16 +0530

    Update right-pane.js

--- src/routes/home/components/right-pane/right-pane.js
+++ src/routes/home/components/right-pane/right-pane.js
@@ -57,7 +57,7 @@ export const HomeRightPane = () => {
     const startTimeRef = useRef()
 
     useEffect(() => {
-        const randomNum = Math.random() * (problems.length - 1)
+        const randomNum = Math.random() * problems.length
         setProblem(problems[Math.floor(randomNum)])
 
         return () => {
