58985baeef861cccc4491495c62baac8b3708770
ashishguptabns <ashishguptabns@gmail.com>
2024-04-01 10:44:37 +0530

    styles

--- src/routes/home/components/right-pane/right-pane.js
+++ src/routes/home/components/right-pane/right-pane.js
@@ -1,87 +1,27 @@
-import styled from "styled-components";
 import { useProblem } from './use-problem';
 import { useTimer } from './use-timer';
+import * as STYLES from './style-right-pane'
 
-export const RefreshButton = styled.div`
-    width: 70px;
-    background: green;
-    color: white;
-    font-size: 14px;
-    border-radius: 10px;
-    padding: 6px;
-    margin-left: 40px;
-    cursor: pointer;
-    text-align: center;
-    height: fit-content;
-    align-self: center;
-`
-export const DoneButton = styled.div`
-    width: 70px;
-    background: #dfd7d7;
-    color: black;
-    font-size: 14px;
-    border-radius: 10px;
-    padding: 6px;
-    margin-left: 10px;
-    cursor: pointer;
-    text-align: center;
-    height: fit-content;
-    align-self: center;
-`
-const ProblemContainer = styled.div`
-    display: flex;
-`
-const Container = styled.div`
-    position: relative;
-    width: 40%;
-    @media (max-width: 700px){
-        display: none;
-    }
-`
-const StickyContainer = styled.div`
-    padding: 40px;
-    line-height: 1.3;
-    white-space: pre-wrap;
-    position: sticky;
-    top: 0;
-`
-const Timer = styled.div`
-    display: flex;
-    align-items: center;
-`
-const TimerCTA = styled.p`
-    border: 1px solid #dcd6d6;
-    padding: 4px 8px;
-    border-radius: 4px;
-    width: fit-content;
-    cursor: pointer;
-    x-shadow: inset 0 -1px 0 #cfc0c0;
-    background: ${props => props.$time > 0 ? '#f47979' : '#e3dfdf'};
-`
-const Time = styled.p`
-    font-size: 16px;
-    margin-left: 20px;
-`
 export const HomeRightPane = () => {
     const [problem, handleRefresh, handleDone] = useProblem()
     const [secondsSpent, handleTimer] = useTimer()
 
-    return <Container>
-        <StickyContainer>
-            <ProblemContainer>
+    return <STYLES.Container>
+        <STYLES.StickyContainer>
+            <STYLES.ProblemContainer>
                 <h2>Solve this Problem</h2>
-                <RefreshButton onClick={handleRefresh}>Shuffle</RefreshButton>
-                <DoneButton onClick={handleDone}>Done</DoneButton>
-            </ProblemContainer>
+                <STYLES.RefreshButton onClick={handleRefresh}>Shuffle</STYLES.RefreshButton>
+                <STYLES.DoneButton onClick={handleDone}>Done</STYLES.DoneButton>
+            </STYLES.ProblemContainer>
             {problem}
-            <Timer>
-                <TimerCTA $time={secondsSpent} onClick={handleTimer}>
+            <STYLES.Timer>
+                <STYLES.TimerCTA $time={secondsSpent} onClick={handleTimer}>
                     {secondsSpent === 0 ? 'Start timer' : 'Stop timer'}
-                </TimerCTA>
-                <Time>
+                </STYLES.TimerCTA>
+                <STYLES.Time>
                     {`${Math.floor(secondsSpent / 60)} m : ${secondsSpent % 60} s`}
-                </Time>
-            </Timer>
-        </StickyContainer>
-    </Container>
+                </STYLES.Time>
+            </STYLES.Timer>
+        </STYLES.StickyContainer>
+    </STYLES.Container>
 }
