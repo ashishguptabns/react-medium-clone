d878554e3577239a0cc73bf559cb5e5b1a59eadb
ashishguptabns <ashishguptabns@gmail.com>
2024-05-05 19:10:47 +0530

    SiteMap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Header } from './ui/header/header';
 import { lazy, Suspense } from 'react';
 import { PlaceholderPage } from './ui/placeholder-page.js';
 import { useInterceptor } from './use-interceptor.js';
+import { SiteMap } from './routes/sitemap/sitemap.js';
 
 const Story = lazy(() => import('./routes/story/story'))
 const Home = lazy(() => import('./routes/home/home'))
@@ -19,6 +20,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/story/:id?" element={<Story />} />
+            <Route path="/sitemap" element={<SiteMap />} />
           </Routes>
         </Suspense>
       </Router>
